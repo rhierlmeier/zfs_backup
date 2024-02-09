3e4cce8da3b2e61e30a2d75fb74600286c5d8348
Richard Hierlmeier <rhierlmeier@gmail.com>
2024-02-09 07:42:26 +0100

    Backup-Prefix eingeführt

--- zfs_backup.py
+++ zfs_backup.py
@@ -13,15 +13,15 @@ __status__ = "Beta"
 # Imports
 # =============================================================================
 import logging
-import re
-import sys
 import os
+import re
+import subprocess
+import sys
 from datetime import datetime
 from typing import List
 
-import yaml
-import subprocess
 import filelock
+import yaml
 
 # =============================================================================
 # Constants
@@ -38,7 +38,8 @@ class Config:
 
     def __init__(self, pool: str, backup_pool: str, datasets: List[str], num_backups_daily: int,
                  num_backups_weekly: int,
-                 num_backups_monthly: int, nums_backup_yearly: int, _log_file: str, _run_file: str):
+                 num_backups_monthly: int, nums_backup_yearly: int, _log_file: str, _run_file: str, _mail_to: str,
+                 _prefix: str):
         self.pool = pool
         self.backup_pool = backup_pool
         self.datasets = datasets
@@ -48,6 +49,8 @@ class Config:
         self.nums_backups_yearly = nums_backup_yearly
         self.log_file = _log_file
         self.run_file = _run_file
+        self.mail_to = _mail_to
+        self.prefix = _prefix
 
     def get_num_backups(self, _backup_type: str):
         if _backup_type == "daily":
@@ -80,12 +83,13 @@ class Diff:
 
 
 class Snapshot:
-    def __init__(self, _pool, _dataset, _backup_pool, _backup_type, _backup_time):
+    def __init__(self, _pool, _dataset, _backup_pool, _backup_type, _backup_time, _prefix):
         self.pool = _pool
         self.dataset = _dataset
         self.backup_pool = _backup_pool
         self.backup_type = _backup_type
         self.backup_time = _backup_time
+        self.prefix = _prefix
 
     def __repr__(self):
         return self.get_full_qualified_snap_name()
@@ -98,7 +102,7 @@ class Snapshot:
         return self.backup_pool + "/" + self.dataset
 
     def get_snap_name(self):
-        return self.backup_type + "-" + self.backup_time.strftime(DATE_TIME_FORMAT)
+        return self.prefix + "-" + self.backup_type + "-" + self.backup_time.strftime(DATE_TIME_FORMAT)
 
     def get_full_qualified_snap_name(self):
         return self.pool + "/" + self.dataset + "@" + self.get_snap_name()
@@ -130,8 +134,8 @@ class Snapshot:
 
 
 def read_config(_config_file):
-    with open(_config_file) as _f:
-        _c = yaml.safe_load(_f)
+    with open(_config_file) as _filehandle:
+        _c = yaml.safe_load(_filehandle)
 
     if _c['dataPool'] is None:
         logging.error("Missing dataPool in " + config_file)
@@ -175,6 +179,16 @@ def read_config(_config_file):
         exit(1)
     _runFile = _c['runFile']
 
+    if _c['prefix'] is None:
+        logging.error("Missing prefix in " + config_file)
+        exit(1)
+    _prefix = _c['prefix']
+
+    if _c['mailTo'] is None:
+        logging.error("Missing mailTo in " + config_file)
+        exit(1)
+    _mail_to = _c['mailTo']
+
     return Config(pool=_c['dataPool'],
                   backup_pool=_c['backupPool'],
                   datasets=_c['datasets'],
@@ -183,7 +197,9 @@ def read_config(_config_file):
                   num_backups_monthly=_monthly,
                   nums_backup_yearly=_yearly,
                   _log_file=_logFile,
-                  _run_file=_runFile)
+                  _run_file=_runFile,
+                  _mail_to=_mail_to,
+                  _prefix=_prefix)
 
 
 def usage():
@@ -239,24 +255,100 @@ def zfs_send(_src, _target, _old=None):
 
 def get_snapshots(_config: Config, _dataset):
     fqn = _config.pool + "/" + _dataset
-    res = subprocess.run(["zfs", "list", "-t", "snapshot", "-H", "-o", "name", fqn],
-                         capture_output=True)
-    if res.returncode != 0:
-        logging.error("Could not determine snapshots of " + fqn)
-        logging.error("STD_ERR:" + str(res.stderr))
-        exit(1)
 
-    regex = re.compile("^.*@([a-z]+)-(" + DATE_TIME_PATTERN + ")$")
+    _res = exec_cmd_and_exit_on_error("zfs", "list", "-t", "snapshot", "-H", "-o", "name", fqn)
+
+    regex = re.compile("^.*@([a-z_]+)-([a-z_]+)-(" + DATE_TIME_PATTERN + ")$")
 
     snaps = []
-    for line in res.stdout.decode("utf-8").splitlines():
+    for line in _res.stdout.decode("utf-8").splitlines():
         result = regex.match(line)
         if result is not None:
-            _backup_type = result.group(1)
+            _prefix = result.group(1)
+            _backup_type = result.group(2)
             _backup_time = datetime.strptime(result.group(2), DATE_TIME_FORMAT)
-            snaps.append(Snapshot(_config.pool, dataset, _config.backup_pool, _backup_type, _backup_time))
+            snaps.append(
+                Snapshot(_config.pool, dataset, _config.backup_pool, _backup_type, _backup_time, _prefix))
 
     return snaps
+
+
+class ZPoolStatus:
+    def __init__(self, _name: str, _available=False, _health="UNAVAIL", _capacity_in_percent=0.0):
+        self.name = _name
+        self.available = _available
+        self.health = _health
+        self.capacity_in_percent = _capacity_in_percent
+
+
+def get_zpool_status(_pool_name: str) -> ZPoolStatus:
+    _res = exec_cmd("zpool", "get", "-H", "-p", "health,capacity", _pool_name)
+
+    _ret = ZPoolStatus(_pool_name)
+
+    if _res.returncode != 0:
+        return _ret
+
+    _ret.available = True
+
+    regex = re.compile("^" + _pool_name + "\\s+([^\\s]+)\\s+([^\\s]+)\\s+")
+
+    for line in _res.stdout.decode("utf-8").splitlines():
+        result = regex.match(line)
+        if result is not None:
+            _property_name = result.group(1)
+            _property_value = result.group(2)
+            if _property_name == "health":
+                # Possible Values ONLINE, DEGRADED, FAULTED, OFFLINE, REMOVED, UNAVAIL
+                _ret.health = _property_value
+            elif _property_name == "capacity":
+                _ret.capacity_in_percent = float(_property_value)
+        else:
+            logging.debug("Line " + line + " does not match " + str(regex))
+
+    return _ret
+
+
+def zpool_import(_pool_name: str):
+    logging.info("Importing ZFS pool " + _pool_name)
+    exec_cmd_and_exit_on_error("zpool", "import", _pool_name)
+
+
+def exec_cmd_and_exit_on_error(*_cmd):
+    logging.debug("Running " + str(_cmd))
+    _res = subprocess.run(_cmd, capture_output=True, shell=False)
+    _return_code = _res.returncode
+    logging.debug("return_code: " + str(_return_code))
+    logging.debug("------- STDOUT ----------------")
+    logging.debug(_res.stdout)
+    logging.debug("------- STDERR ----------------")
+    logging.debug(_res.stderr)
+
+    _res = subprocess.run(_cmd, capture_output=True, shell=False)
+
+    if _return_code != 0:
+        logging.error("Command [" + str(_cmd) + "] exited with return code " + str(_return_code)
+                      + "\n: stderr:" + str(_res.stderr))
+        exit(1)
+
+    return _res
+
+
+def exec_cmd(executable: str, *_args):
+    _cmd = [executable] + list(_args)
+    logging.debug("Running " + str(_cmd))
+    _res = subprocess.run(_cmd, capture_output=True, shell=False)
+    logging.debug("return_code: " + str(_res.returncode))
+    logging.debug("------- STDOUT ----------------")
+    logging.debug(_res.stdout)
+    logging.debug("------- STDERR ----------------")
+    logging.debug(_res.stderr)
+    return _res
+
+
+def zpool_export(_pool_name: str):
+    logging.info("Exporting ZFS pool " + _pool_name)
+    exec_cmd_and_exit_on_error("zpool", "export", _pool_name)
 
 
 def backup(_config: Config, _dataset: str, _backup_type: str):
@@ -265,6 +357,32 @@ def backup(_config: Config, _dataset: str, _backup_type: str):
     logging.info("Backup [{pool}]/{dataset}@{backup_type}] to {backup_pool} (num backups to keep={num_backups_to_keep})"
                  .format(pool=_config.pool, dataset=_dataset, backup_type=_backup_type, backup_pool=_config.backup_pool,
                          num_backups_to_keep=_num_backups_to_keep))
+
+    _poolStatus = get_zpool_status(_config.pool)
+
+    if not _poolStatus.available:
+        zfs_backup_failed(_config, "zpool " + _config.pool + " is not available")
+
+    if _poolStatus.health != "ONLINE":
+        zfs_backup_failed(_config, "zpool " + _config.pool + " is not healthy (health=" + _poolStatus.health + ")")
+
+    _backupPoolStatus = get_zpool_status(_config.backup_pool)
+
+    _backupPoolImport = False
+
+    if not _backupPoolStatus.available:
+        zpool_import(_config.backup_pool)
+        _backupPoolImport = True
+        _backupPoolStatus = get_zpool_status(_config.backup_pool)
+
+    if _backupPoolStatus.health != "ONLINE":
+        zfs_backup_failed(_config,
+                          "backup zpool " + _config.backup_pool + " is not healthy (health="
+                          + _backupPoolStatus.health + ")")
+
+    if _backupPoolStatus.capacity_in_percent < 0.2:
+        zfs_backup_warn(_config, "Low capacity " + str(_backupPoolStatus.capacity_in_percent)
+                        + " in pool " + _backupPoolStatus.name)
 
     _all_snapshots = get_snapshots(_config, _dataset)
 
@@ -286,11 +404,43 @@ def backup(_config: Config, _dataset: str, _backup_type: str):
         _snapshots.pop(0).destroy()
 
     if len(_snapshots) == 0:
-        new_snap = Snapshot(_config.pool, dataset, _config.backup_pool, _backup_type, datetime.now())
+        new_snap = Snapshot(_config.pool, dataset, _config.backup_pool, _backup_type, datetime.now(), _config.prefix)
         new_snap.create(_last_snapshot)
     else:
-        new_snap = Snapshot(_config.pool, dataset, _config.backup_pool, _backup_type, datetime.now())
+        new_snap = Snapshot(_config.pool, dataset, _config.backup_pool, _backup_type, datetime.now(), _config.prefix)
         new_snap.create(_last_snapshot)
+
+    if _backupPoolImport:
+        zpool_export(_config.backup_pool)
+
+
+def sendmail(_config: Config, body, subject):
+    sendmail_location = "/usr/sbin/sendmail"  # sendmail location
+    p = os.popen("%s -t" % sendmail_location, "w")
+    p.write("To: %s\n" % "news@hierlmeier.de")
+    p.write("Subject: " + subject + "\n")
+    p.write("\n")  # blank line separating headers from body
+    p.write(body)
+    status = p.close()
+    if status != 0:
+        print("Sendmail exit status", status)
+
+
+def zfs_backup_failed(_config: Config, _msg: str):
+    logging.error(_msg)
+    sendmail(_config, """ZFS backup failed 
+   {msg}
+   
+   """.format(msg=_msg), "ZFS backup failed")
+    exit(1)
+
+
+def zfs_backup_warn(_config: Config, _msg: str):
+    logging.warning(_msg)
+    sendmail(_config, """Warning from ZFS backup 
+   {msg}
+
+   """.format(msg=_msg), "Warning from ZFS backup")
 
 
 if __name__ == '__main__':
@@ -364,5 +514,3 @@ if __name__ == '__main__':
             _f.write("")
 
         logging.info("Backup finished --------------------------------")
-
-
